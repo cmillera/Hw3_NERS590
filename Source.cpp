1819fa46222faa546839b7a7867500af865a884a
cmillera <cmillera@umich.edu>
2017-04-19 17:35:47 -0400

    Revert "Added all file, final HW3"
    This reverts commit 36109bc7b618fea74b621455d5cbaf366b5ab875.

--- Source.cpp
+++
@@ -1,8 +0,0 @@
-#include "Source.h"
-
-std::stack<particle> source::sample() {
-  std::stack<particle> pbank;
-  //std::cout<<dist_pos->sample()<<std::endl;
-  pbank.push( particle( dist_pos->sample(), dist_dir->sample() ) );
-  return pbank;
-}
